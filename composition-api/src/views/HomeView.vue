3a07d82c0ffb4465f36cebc315181071b2c5cca6
tarikyesilcimenn <tarikyesilcimenn@gmail.com>
2022-10-14 14:51:05 +0300

    search filter
    search filter with vue3

---
+++ composition-api/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <input type="text" v-model="searched">
+      <div v-for="name in finded" :key="name">
+        {{name}}
+      </div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup () {
+    const names = ref(['tarik', 'hakan', 'sedat', 'zehra', 'hacer'])
+    const searched = ref('')
+    const finded = computed(() => {
+      return names.value.filter((name) => name.includes(searched.value))
+    })
+    return {
+      names,
+      searched,
+      finded
+    }
+  }
+}
+</script>
